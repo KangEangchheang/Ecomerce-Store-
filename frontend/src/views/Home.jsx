007e69fdc0e52f456a53e94d8ba36cc41c01e65e
Kang Eangchheang <eangchheangkang@gmail.com>
2023-11-21 20:41:54 +0700

    new

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -6,13 +6,13 @@ export default function Home(){
     
     return(
         <>
+            {/* Header Banner Section */}  
             <div className="mt-2 h-[70vh]">
                 <div className="bg-[#0A0A0A] w-screen h-[70vh] absolute left-0"></div>
             </div>
             <div className="flex flex-col gap-16 mt-16">
-                {/* Header Banner Section */}
                 <Category></Category>
-                {/* <Card bg={'red'}></Card> */}
+                
             </div>
         </>
         
