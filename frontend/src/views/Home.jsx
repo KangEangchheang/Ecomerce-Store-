7eb13c8c509f9101c058dfecacc1ccf02edd2d01
chharng-chhit <nooberchhit@gmail.com>
2023-11-21 20:25:33 +0700

    cart

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -7,9 +7,9 @@ export default function Home(){
     return(
         <div className="flex flex-col gap-16 mt-2">
             {/* Header Banner Section */}
-            <div className="ml-[-7vw] w-[100vw] bg-[#0A0A0A] h-[80vh]"></div>
+            {/* <div className="ml-[-7vw] w-[100vw] bg-[#0A0A0A] h-[80vh]"></div> */}
             <Category></Category>
-            {/* <Card bg={'red'}></Card> */}
+            <Card bg='red'></Card>
         </div>
     )
 }
